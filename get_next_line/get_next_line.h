0d6fa543666c4a79c2d7a2e13c9290d30a0cd275
SickestOne <117588696+SickestOne@users.noreply.github.com>
2023-02-28 23:19:59 +0100

    upd

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: rvan-den <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 16:16:22 by rvan-den          #+#    #+#             */
-/*   Updated: 2023/02/28 21:47:25 by pendejoo         ###   ########.fr       */
+/*   Updated: 2023/02/28 23:18:46 by pendejoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
+#  define BUFFER_SIZE 10
 # endif 
 
 # include <unistd.h>
