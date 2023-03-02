e02ff4c3a97bb5c75351ca0c71a0eae14fb0add1
SickestOne <117588696+SickestOne@users.noreply.github.com>
2023-03-02 16:52:49 +0100

    start
    started

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,8 +6,22 @@
 /*   By: pendejoo </var/spool/mail/pendejoo>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 21:15:18 by pendejoo          #+#    #+#             */
-/*   Updated: 2023/03/01 21:15:24 by pendejoo         ###   ########.fr       */
+/*   Updated: 2023/03/02 16:41:50 by rvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// arguments du tableau
+// write tableau d'int avant modif
+// actions sur tableauu d'int
+// write tableau d'int apres modifications
 
+# include "push_swap.h"
+
+int	main(int argc,char **argv)
+{
+	if (argc == 2)
+		printf("%d", ft_atoi(argv[1]));
+	else
+		return (0);
+	return (0);
+}
