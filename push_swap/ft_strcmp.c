951dfae033c8f9dc8be0eede4e4920a2a7cb1df6
SickestOne <117588696+SickestOne@users.noreply.github.com>
2023-03-09 18:31:15 +0100

    error when double but segfault when ok...

--- push_swap/ft_strcmp.c
+++ push_swap/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: rvan-den <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 15:48:51 by rvan-den          #+#    #+#             */
-/*   Updated: 2023/03/09 17:44:30 by pendejoo         ###   ########.fr       */
+/*   Updated: 2023/03/09 18:29:54 by pendejoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,19 @@ int	ft_strcmp(int argc, char **str)
 	j = 0;
 	k = 1;
 	temp = str;
-	while (str[i] && k < argc)
+	while (str[i])
 	{
 		while (str[i][j])
 		{
 			if (argc  == 2)
 				return (1);
-			else if ()
-				return (0);
-			else
+			else if ((str[i + 1][j] != temp[k][j] && str[i + 1][j] != '\0'))
 				j++;
+			else
+				return (0);
 		}
 		j = 0;
 		i++;
-		k++;
 	}
 	return (1);
 }
