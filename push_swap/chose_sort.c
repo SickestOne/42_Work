9839ca5877b693b53312e1c69dbd02e01bc7a2da
SickestOne <117588696+SickestOne@users.noreply.github.com>
2023-03-26 19:49:25 +0200

    update

--- push_swap/chose_sort.c
+++ push_swap/chose_sort.c
@@ -6,7 +6,7 @@
 /*   By: pendejoo </var/spool/mail/pendejoo>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 17:35:58 by pendejoo          #+#    #+#             */
-/*   Updated: 2023/03/26 19:35:45 by pendejoo         ###   ########.fr       */
+/*   Updated: 2023/03/26 19:44:51 by pendejoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,11 @@ int	chose_sort_sa(char **str)
 
 	temp = ft_split((const char *)str[1], ' ');
 	len = sa_tab_len(temp);
+	printf("nbr sa args : %d\n", len);
 	if (len >= 2 && len <= 5)
-		printf("SA Use algo for len between 2 and 5\n");
+		printf("ac == 2 Use algo for len between 2 and 5\n");
 	else if (len > 5)
-		printf("SA Use algo for big ass lists\n");
+		printf("ac == 2 Use algo for big ass lists\n");
 	return (0);
 }
 
@@ -31,10 +32,11 @@ int	chose_sort_ma(char **str)
 	int len;
 
 	len = ma_tab_len(str);
+	printf("nbr ma args : %d\n", len);
 	if (len >= 2 && len <= 5)
-		printf("MA Use algo for len between 2 and 5\n");
+		printf("ac > 2 Use algo for len between 2 and 5\n");
 	else if (len > 5)
-		printf("MA Use algo for big ass lists\n");
+		printf("ac > 2 Use algo for big ass lists\n");
 	return (0);
 }
 
