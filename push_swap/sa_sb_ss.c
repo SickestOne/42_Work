29b0bea058a7e773def31421a17633c5afe63768
SickestOne <117588696+SickestOne@users.noreply.github.com>
2023-03-28 23:20:38 +0200

    update (sa_sb_ss)

--- push_swap/sa_sb_ss.c
+++ push_swap/sa_sb_ss.c
@@ -6,7 +6,7 @@
 /*   By: rvan-den <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 16:47:45 by rvan-den          #+#    #+#             */
-/*   Updated: 2023/03/27 17:49:32 by rvan-den         ###   ########.fr       */
+/*   Updated: 2023/03/28 20:50:13 by pendejoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,13 @@ t_ctrl *swap_a(t_ctrl *ab)
 {
 	t_node *tmp;
 
-	tmp = ab->a->prev->prev;
+	tmp = malloc(sizeof(t_node));
+	if (!tmp)
+		return (ab);
+	ab = go_top(ab);
+	tmp = ab->a->next->prev;
 	printf("tmp val: %d\n", tmp->data);
+	ab->a = ab->a->next;
+	ab->a->prev = NULL;
 	return (ab);
 }
