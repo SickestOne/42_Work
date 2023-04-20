3fc1c793afe3ca885ae9e861582a62cfb8163edb
SickestOne <117588696+SickestOne@users.noreply.github.com>
2023-04-20 22:21:49 +0200

    com sort_big_utils.c

--- push_swap/sort_big_utils.c
+++ push_swap/sort_big_utils.c
@@ -6,7 +6,7 @@
 /*   By: rvan-den <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 16:22:48 by rvan-den          #+#    #+#             */
-/*   Updated: 2023/04/20 20:56:25 by pendejoo         ###   ########.fr       */
+/*   Updated: 2023/04/20 22:16:49 by pendejoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,19 @@ void	fill_b(t_ctrl *ab, int inter_max, int inter_min)
 
 	while (a_has_rank(ab, inter_max, inter_min) != 1)
 	{
-		/*total_len = count_nodes_b(ab);
-		if ((find_max_data_b(ab) >= inter_min
-			&& find_max_data_b(ab)<= inter_max) 
-			&& (find_max_pos_b(ab) > (total_len / 2)))
-			fill_b_case_1(ab, total_len);*/
+		// if (find_max_rank >= i_min && find_max_rank <= i_max)
+		//		if (find_max_pos > tot_len / 2 + 1)
+		//			while (find_max_pos < total_len)
+		//				rra;
+		//			pb;
+		//		else if (find_max_pos < total_len / 2 + 1)
+		//			while(find_max_pos > 1)
+		//				ra;
+		//			pb;
+		//		else if (ab->a->rank >= i_min && ab->a->rank <= i_max)
+		//			pb;
+		//		else
+		//			rotate_a;
 		if (ab->a->rank >= inter_min && ab->a->rank <= inter_max)
 			push_b(ab, 1);
 		else
