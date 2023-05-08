f7319e2da2be97c84a6fe4a61e2b749287b11aed
SickestOne <117588696+SickestOne@users.noreply.github.com>
2023-05-08 16:20:15 +0200

    update example .txt

--- pipex/parent.c
+++ pipex/parent.c
@@ -6,25 +6,20 @@
 /*   By: rvan-den <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 14:29:39 by rvan-den          #+#    #+#             */
-/*   Updated: 2023/05/05 14:14:24 by rvan-den         ###   ########.fr       */
+/*   Updated: 2023/05/08 13:35:43 by rvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(void)
-{
-  pid_t          pid;
 
-  if ((pid = fork()) == -1)
-    {
-      perror("fork");
-      return 1;
-    }
-  else if (pid == 0)
-    printf("Je suis le fils, et mon pid = %d\n", getpid());
-  else
-    printf("Je suis le pere, et mon pid = %d. Le pid de mon fils = %d\n", getpid(), pid);
+int main(int argc, char *argv[], char *env[])
+{
+  if (argc > 1)
+    if (execve(argv[1], argv + 1, env) == -1)
+      perror("execve");
+
+  printf("My pid is: %d\n", getpid());
 
   return 0;
 }
