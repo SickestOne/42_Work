c864f36522c164149bbf5a2f47d3abeaa9d55ce3
SickestOne <117588696+SickestOne@users.noreply.github.com>
2023-05-11 22:44:11 +0200

    update

--- pipex/parent.c
+++ pipex/parent.c
@@ -6,20 +6,19 @@
 /*   By: rvan-den <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 14:29:39 by rvan-den          #+#    #+#             */
-/*   Updated: 2023/05/08 13:35:43 by rvan-den         ###   ########.fr       */
+/*   Updated: 2023/05/11 17:54:29 by pendejoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-
-int main(int argc, char *argv[], char *env[])
+int	main()
 {
-  if (argc > 1)
-    if (execve(argv[1], argv + 1, env) == -1)
-      perror("execve");
-
-  printf("My pid is: %d\n", getpid());
-
-  return 0;
+	int	file = open("result.txt", O_WRONLY | O_CREAT, 0777);
+	if (file == -1)
+		return 2;
+	printf ("fd de result.txt : %d\n", file);
+	int file2 = dup2(file, STDOUT_FILENO);
+	printf ("%d\n", file2);
+	return (0);
 }
