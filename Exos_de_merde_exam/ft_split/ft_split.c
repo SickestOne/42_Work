669028b1c4179af932aa6e60e99b7316f236ccd2
SickestOne <117588696+SickestOne@users.noreply.github.com>
2023-06-14 17:34:54 +0200

    Guillaume the 🐐

---
+++ Exos_de_merde_exam/ft_split/ft_split.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_issp(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n' || c == '\0')
+		return (1);
+	return (0);
+}
+
+int	word_c(char *str)
+{
+	int i = 0;
+	int wc = 0;
+	while (str[i])
+	{
+		if (!ft_issp(str[i]) && ft_issp(str[i + 1]))
+			wc++;
+		i++;
+	}
+	return (wc);
+}
+
+char *find_w(char *str)
+{
+	static int i = 0;
+	int j = 0;
+	int k = i;
+	char *tmp;
+	while (!ft_issp(str[i]) && str[i])
+		i++;
+	tmp = malloc(sizeof(char) * (i - k + 1));
+	if (!tmp)
+		return (NULL);
+	tmp[i - k] = 0;
+	j = 0;
+	while (k < i)
+		tmp[j++] = str[k++];
+	if (str[i])
+		i++;
+	return (tmp);
+}
+
+char **ft_split(char *str)
+{
+	int i = 0;
+	int count_c = word_c(str);
+	char **tab;
+	tab = malloc(sizeof(char *) * (count_c + 1));
+	if (!tab)
+		return (NULL);
+	tab[count_c] = NULL;
+	while (count_c != 0)
+	{
+		tab[i] = find_w(str);
+		count_c --;
+		i++;
+	}
+	return (tab);
+}
+
+int	main()
+{
+	char **split = ft_split("coucou les gens");
+	int i;
+	for (i = 0; split[i]; i++)
+		printf("split[%d] = %s\n", i, split[i]);
+	printf("split[%d] = %s\n", i, split[i]);
+	return (0);
+}
